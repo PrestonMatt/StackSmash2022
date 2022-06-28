822f7892bda014da598a5bca5b2a4242768a4d3e
Matthew Preston <46906782+PrestonMatt@users.noreply.github.com>
2022-06-28 15:36:42 -0400

    gdd exploit allows stack static addressing

---
+++ example3update.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void ohno(void);
+
+void main(){
+    printf("Put in your exploit!\n\n");
+    printf(">");
+    
+    ohno();
+}
+
+void ohno(){ //"Oh no! I've made a coding error" -Nobody, ever.
+    char large_string[64];
+    char small_string[16];
+    
+    fgets(large_string,64,stdin);
+    //printf("\nYou put this input in, %s at address %d\n", large_string, &large_string);
+    
+    printf("Copying memory...%s into a blank buffer...", large_string, small_string);
+    memcpy(small_string,large_string,64);
+    asm volatile(
+        "ret\n\t"
+    );
+    //strcpy(small_string,large_string);
+}
